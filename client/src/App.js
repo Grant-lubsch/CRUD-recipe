d4cb716dbd43e79716cb59e68434fc60ad4a0a4b
Grant Lubsch <grant.lubsch@gmail.com>
2023-04-16 10:51:26 -0500

    added axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,79 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import Axios from "axios";
 
 function App() {
+  const [recipeName, setRecipeName] = useState("");
+  const [recipeTime, setRecipeTime] = useState("");
+  const [recipeIngredients, setRecipeIngredients] = useState("");
+  const [recipeInstructions, setRecipeInstructions] = useState("");
+  const [recipeList, setRecipeList] = useState([]);
+
+  useEffect(() => {
+    Axios.get("http://localhost:3003/api/get").then((response) => {
+      setRecipeList(response.data);
+    });
+  }, []);
+
+  const submitRecipe = () => {
+    Axios.post("https://localhost:3003/api/insert", {
+      recipeName: recipeName,
+      recipeTime: recipeTime,
+      recipeInstructions: recipeInstructions,
+      recipeIngredients: recipeIngredients,
+    }).then(() => {
+      alert("Recipe successfully");
+    });
+  };
+
   return (
     <div className="App">
-      <h1>CRUD Recipe's</h1>
+      <h1>CRUD Recipes</h1>
       <div className="form">
         <label>Recipe:</label>
-        <input type="text" name="recipeName" />
+        <input
+          type="text"
+          name="recipeName"
+          onChange={(e) => {
+            setRecipeName(e.target.value);
+          }}
+        />
         <label>Time to Cook:</label>
-        <input type="text" name="recipeTime" />
+        <input
+          type="text"
+          name="recipeTime"
+          onChange={(e) => {
+            setRecipeTime(e.target.value);
+          }}
+        />
         <label>Ingredients:</label>
-        <input type="text" name="recipeIngredients" />
+        <input
+          type="text"
+          name="recipeIngredients"
+          onChange={(e) => {
+            setRecipeIngredients(e.target.value);
+          }}
+        />
         <label>Instructions:</label>
-        <input type="text" name="recipeInstructions" />
+        <input
+          type="text"
+          name="recipeInstructions"
+          onChange={(e) => {
+            setRecipeInstructions(e.target.value);
+          }}
+        />
 
-        <button>Submit</button>
+        <button onClick={submitRecipe}>Submit</button>
+
+        {recipeList.map((val) => {
+          return (
+            <h1>
+              Recipe Name: {val.recipeName} | Time to Cook: {val.recipeTime} |
+              Recipe Ingredients: {val.recipeIngredients} | Recipe Instructions:{" "}
+              {val.recipeInstructions}
+            </h1>
+          );
+        })}
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,8 +1,42 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
+const mysql = require("mysql");
 
-app.get("/", (req, res) => {
-  res.send("hello grant");
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "AsDfGh12!",
+  database: "CRUD_Recipes",
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/api/get", (req, res) => {
+  const sqlSelect = "SELECT * FROM recipes";
+  db.query(sqlSelect, (err, result) => {
+    res.send(result);
+  });
+});
+
+app.post("/api/insert", (req, res) => {
+  const recipeName = req.body.recipeName;
+  const recipeTime = req.body.recipeTime;
+  const recipeIngredients = req.body.recipeIngredients;
+  const recipeInstructions = req.body.recipeInstructions;
+
+  const sqlInsert =
+    "INSERT INTO recipes (recipeName, recipeTime, recipeIngredients, recipeInstructions) VALUES (?, ?, ?, ?)";
+  db.query(
+    sqlInsert,
+    [recipeName, recipeTime, recipeIngredient, recipeInstructions],
+    (err, result) => {
+      console.log(result);
+    }
+  );
 });
 
 app.listen(3003, () => {
