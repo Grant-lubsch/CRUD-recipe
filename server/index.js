90559e4b956b21d3f2e332e52bb0c95fe418885d
Grant Lubsch <grant.lubsch@gmail.com>
2023-05-23 23:29:20 -0500

    adding static client code created by npm run build to the server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 const app = express();
 const mysql = require("mysql");
 //const PORT = process.env.PORT || 3030;
@@ -14,7 +15,12 @@ const db = mysql.createPool({
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "build")));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.get("/api/get", (req, res) => {
   const sqlSelect = "SELECT * FROM recipes";
