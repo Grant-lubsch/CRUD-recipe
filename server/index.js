fa373f2f630e7477f3b3ff064b2977d70d873787
Grant Lubsch <grant.lubsch@gmail.com>
2023-05-24 00:12:56 -0600

    attempting to fix wierd routing issue with Heroku where we get a timeout on /api/get/

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const db = mysql.createPool({
 app.use(cors());
 app.use(express.static(path.join(__dirname, "build")));
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
